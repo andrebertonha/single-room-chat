39e0740ea9fb9985534f4619beb9ea3517dbdb6f
André Martins <andrebertonhadev@gmail.com>
2019-11-22 20:38:43 -0300

    update: cluster support

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 'use strict'
 
+const cluster = require('cluster')
+
+if (cluster.isMaster) {
+  for (let i=0; i < 4 i++) {
+    cluster.fork()
+  }
+
+  require('@adonisjs/websocket/clusterPubSub')()
+  return
+}
+
 const { Ignitor } = require('@adonisjs/ignitor')
 
 new Ignitor(require('@adonisjs/fold'))
